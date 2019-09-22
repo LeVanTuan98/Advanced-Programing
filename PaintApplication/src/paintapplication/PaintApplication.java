924bc48f6718558fd9a55633c2050de659913c3f
levantuan98 <tuan.lv164352@sis.hust.edu.vn>
2019-09-22 22:34:45 +0700

    22-9-19

---
+++ PaintApplication/src/paintapplication/PaintApplication.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paintapplication;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+
+/**
+ *
+ * @author Admin
+ */
+public class PaintApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       JFrame PaintForm = new PaintForm_JFrame();
+       PaintForm.setVisible(true);
+        
+    }
+    
+}
